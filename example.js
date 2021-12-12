e69bba7b936aa7cf495bd759b5b19e319294e00a
WKurnawan <92246758+wkurnawan@users.noreply.github.com>
2021-12-12 22:52:09 +0700

    Belajar JS

---
+++ example.js
@@ -0,0 +1,38 @@
+// const bulan = [
+//   "Jan",
+//   "Feb",
+//   "Mar",
+//   "April",
+//   "May",
+//   "Jun",
+//   "Jul",
+//   "August",
+//   "Sept",
+//   "Okt",
+//   "Nov",
+//   "Des",
+// ];
+// const tanggal = new Date();
+// let month = bulan[tanggal.getMonth()];
+
+//Variabel di Javascript
+var _nama = "Asrul Abdullah";
+// nama = "Budiman"; // isi ulang variabel
+// delete nama; //hapus variabel
+var alamat_kamu = "Jaln Pramuka"; //Snake Case
+var jnsKelamin = "Laki-Laki"; //CameCase
+alert("Welcome");
+
+let harga = 15000;
+biaya = 20000;
+//Pada Js disarankan untuk menggunakan variabel dengna model camelCase
+
+//Menampilkan isi variabel
+//console.log() menampilkan output ke console log javascript
+//document.write() menampilkan output ke dokumen HTML
+//alert() menampilkan output ke jendela dialog
+
+//Tipe Data
+// Aturan penulisan nama variabel = tidak boleh angka di depan
+// Dianjurkan gaya penulisan variabel pakai CamelCase
+// boleh diawal menggunakan underscore
